41002f4f4537095c68ced670fe397857baccb473
sebe <sebe2k04@gmail.com>
2024-03-18 19:21:25 +0530

    about and bg added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import "./Navbar.scss";
-import sebe from '../../assets/Images/favicon.png'
+import sebe from "../../assets/Images/favicon.png";
 import { useState } from "react";
 import Drawer from "@mui/material/Drawer";
+import { motion } from "framer-motion";
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
@@ -9,63 +11,86 @@ const Navbar = () => {
     setOpen(newOpen);
   };
   return (
-    <div className="navbar  w-full md:w-full  text-xl text-gray-500 lato-regular">
-      <nav className="flex justify-between">
-        <div className="pl-10 pt-7 md:pt-4">
-          <img src={sebe} className="w-[60px] grayscale" alt="" />
-        </div>
-        <div className="links pt-10 gap-5 md:gap-8 tracking-wide hidden md:flex">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Skills</a>
-          <a href="">Projects</a>
-        </div>
-        <div className="links  pt-10 pr-10 hidden md:flex">
-          <a href="" className="links">
-            Contact
-          </a>
-        </div>
-        <div className="ham-menu flex md:hidden mt-7 mr-5">
+    <div className="navbarfull bg-transparent pb-4">
+      <div className="navbar  w-full md:w-full  text-xl text-gray-500 lato-regular">
+        <nav className="flex justify-between">
+          <div className="pl-10 pt-4 md:pt-4">
+            <img src={sebe} className="w-[60px] grayscale" alt="" />
+          </div>
+          <div className="links h-fit pt-8 gap-5 md:gap-8 tracking-wide hidden md:flex">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+          </div>
+          <div className="links pt-8 pr-10 h-fit hidden md:flex">
+            <a href="" className="links">
+              Contact
+            </a>
+          </div>
+          <div className="ham-menu flex md:hidden mt-7 mr-5">
             <button onClick={toggleDrawer(true)} className="">
               <svg
+              className=""
+                width="40px"
+                height="40px"
+                viewBox="0 0 24 24"
+                fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 14 14"
               >
                 <path
-                  fill="none"
-                  stroke="#ffffff"
+                  d="M4 6H20M4 12H14M4 18H9"
+                  stroke="#000000"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M13.5 2H.5m13 5H.5m13 5H.5"
                 />
               </svg>
             </button>
           </div>
-      </nav>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-          <div className="drawer">
-          <svg width="800px" height="800px" onClick={toggleDrawer(false)} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="closeIcon"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z" fill="#000000"/></svg>
-          <li>
-          <a href="">Home</a>
-          </li>
-          <li>
-          <a href="">About</a>
-          </li>
-          <li>
-          <a href="">Skills</a>
-          </li>
-          <li>
-          <a href="">Projects</a>
-          </li>
-          <li>
-          <a href="">Contact</a>
-          </li>
-          </div>
+        </nav>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <motion.div
+            className="drawer"
+            initial={{ opacity: 0, scale: 0.8, x: -50 }}
+            whileInView={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 1.2 }}
+          >
+            <svg
+              width="800px"
+              height="800px"
+              onClick={toggleDrawer(false)}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="closeIcon"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z"
+                fill="#000000"
+              />
+            </svg>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Skills</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </motion.div>
         </Drawer>
+      </div>
     </div>
-    
   );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,20 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Home.scss";
-import { useInView } from "framer-motion";
-//
+import { motion, useInView } from "framer-motion";
+import backgroundvideo from "../../assets/Videos/bg2.mp4";
+import pic from "../../assets/Images/picc.png";
 
 const Home = () => {
   const ref = useRef(null);
   const isInView = useInView(ref);
+
   return (
     <div className="">
-      <div className="h-[100vh] flex justify-center">
-        <div className="pt-40 text-gray-500 duration-500 hover:text-black">
+      <div className="h-[90vh] md:h-[100vh] flex justify-center">
+        <video autoPlay loop muted id="video" className="video">
+          <source src={backgroundvideo} type="video/mp4" />
+        </video>
+        <div className="pt-40 text-gray-400 duration-500 ">
           <h4
             className="dancing-script text-2xl md:text-3xl"
             ref={ref}
@@ -32,28 +37,43 @@ const Home = () => {
           >
             Sebe S
           </h1>
-          <h3 className="dancing-script text-right text-3xl md:text-5xl" ref={ref}
+          <h3
+            className="dancing-script text-right text-3xl md:text-5xl"
+            ref={ref}
             style={{
               transform: isInView ? "none" : "translateY(200px)",
               opacity: isInView ? 1 : 0,
               transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-            }}>
+            }}
+          >
             Web Developer
           </h3>
         </div>
       </div>
-      <div id="about" className="about bg-gray-400 h-[100vh] text-white">
-        <p>
-          Welcome to my digital space! I am Sebe S, a passionate web developer
-          with a love for turning ideas into interactive and visually appealing
-          experiences. This portfolio is a reflection of my journey, featuring a
-          collection of projects that highlight my skills in web development,
-          design, etc.. From crafting elegant and responsive websites to solving
-          complex problems through innovative solutions, I'm dedicated to
-          creating meaningful and impactful digital experiences. Explore my
-          work, and let's connect to discuss how we can collaborate and bring
-          your ideas to life. Thank you for visiting!
-        </p>
+      <div id="about" className="about md:pl-[100px] md:pr-[100px]">
+        <div className=" md:grid grid-cols-2 gap-10">
+          <div className="flex justify-center">
+            <img src={pic} alt="" className="rounded-xl shadow-xl h-[400px] " />
+          </div>
+          <div className="pt-20">
+            <motion.p className=""
+            initial={{ opacity: 0, scale: 0.8, x: -50 }}
+            whileInView={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 1.2 }}
+            >
+              Welcome to my digital space! I am Sebe S, a passionate web
+              developer with a love for turning ideas into interactive and
+              visually appealing experiences. This portfolio is a reflection of
+              my journey, featuring a collection of projects that highlight my
+              skills in web development, design, etc.. From crafting elegant and
+              responsive websites to solving complex problems through innovative
+              solutions, I'm dedicated to creating meaningful and impactful
+              digital experiences. Explore my work, and let's connect to discuss
+              how we can collaborate and bring your ideas to life. Thank you for
+              visiting!
+            </motion.p>
+          </div>
+        </div>
       </div>
     </div>
   );
