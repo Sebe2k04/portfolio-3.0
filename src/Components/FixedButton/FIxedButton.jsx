e879d7f9e635077b6e1406e01560c9fef26e6fed
sebe <sebe2k04@gmail.com>
2024-03-19 21:57:26 +0530

    #1.6 feat spin button added

--- src/Components/FixedButton/FIxedButton.jsx
+++ src/Components/FixedButton/FIxedButton.jsx
@@ -6,8 +6,8 @@ const FIxedButton = () => {
   return (
     <div>
       <div className="fixedlogo fixed z-100 right-6 bottom-7 bg-white">
-        <img src={logo} alt="" className="fimg1" />
-        <img src={devlogo} alt="" className="fimg2 animate-spin" />
+        <img src={logo} alt="" className="fimg1 w-[60px] md:w-[80px]" />
+        <img src={devlogo} alt="" className="fimg2 animate-spin w-[60px] md:w-[80px]" />
       </div>
     </div>
   );
